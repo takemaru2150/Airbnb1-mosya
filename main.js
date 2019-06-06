9195698a87de6e913367d2e26757e300eecb7c41
sakurai <takeshi2jp1000@gmail.com>
2019-06-06 15:01:04 +0900

    Airbnbを始めてgitに追加しました

---
+++ main.js
@@ -0,0 +1,10 @@
+// var alertBtn = document.getElementById('alertbtn');
+//
+// alertBtn.addEventListener('click', function() {
+//   $('#.alert').alert('close')
+// })
+
+
+$("#myAlert").on('click',function(){
+  $("#myAlert").alert();
+});
